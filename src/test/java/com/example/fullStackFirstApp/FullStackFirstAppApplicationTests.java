2584b7ece62a8819fe3c5bca56861eea22c4aecd
PedroGhost007 <prcpinto.04@gmail.com>
2024-01-14 19:28:22 -0300

    Primeiro commit: Conteúdo visto até a aula 9

---
+++ src/test/java/com/example/fullStackFirstApp/FullStackFirstAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fullStackFirstApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStackFirstAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
